a8d346233dd4d6cf22f3c50bff44708e3a7f4848
Ke, Mingze <mingze.ke@gmail.com>
2018-01-27 10:08:22 +0800

    Fix JSONP object

--- JSONP.js
+++ JSONP.js
@@ -1,48 +1,47 @@
-+(function(factory) {
-    if (typeof exports === 'undefined') {
-        factory(webduino || {});
-    } else {
-        module.exports = factory;
++(function (factory) {
+  if (typeof exports === 'undefined') {
+    factory(webduino || {});
+  } else {
+    module.exports = factory;
+  }
+}(function (scope) {
+  'use strict';
+
+  var proto;
+  var Module = scope.Module;
+
+  function JSONP(fetchURL) {
+    Module.call(this);
+    this._fetchURL = fetchURL;
+    this._jsonList = [];
+  }
+
+  JSONP.prototype = proto = Object.create(Module.prototype, {
+    constructor: {
+      value: JSONP
     }
-}(function(scope) {
-    'use strict';
+  });
 
-    var self;
-    var proto;
-    var Module = scope.Module;
+  proto.fetch = function (cb) {
+    $.getJSON(this._fetchURL, function (json) {
+      this._jsonList = json;
+      cb();
+    }.bind(this));
+  }
 
-    function JSONP(fetchURL) {
-        Module.call(this);
-        self = this;
-        self._fetchURL = fetchURL;
-        self._jsonList = [];
+  proto.data = function () {
+    return this._jsonList;
+  }
+
+  proto.dataFilter = function (key, val, dataKey) {
+    for (var i in this._jsonList) {
+      var obj = this._jsonList[i];
+      if (obj[key] == val) {
+        return obj[dataKey];
+      }
     }
+    return "";
+  }
 
-    JSONP.prototype = proto = Object.create(Module.prototype, {
-        constructor: {
-            value: JSONP
-        }
-    });
-
-    proto.fetch = function(cb) {
-        $.getJSON(self._fetchURL, function(json) {
-            self._jsonList = json;
-            cb();
-        });
-    }
-
-    proto.data = function() {
-        return self._jsonList;
-    }
-
-    proto.dataFilter = function(key,val,dataKey){
-        for(var i in self._jsonList){
-            var obj = self._jsonList[i];
-            if(obj[key]==val){
-                return obj[dataKey];
-            }
-        }
-        return "";
-    }
-    scope.module.JSONP = JSONP;
+  scope.module.JSONP = JSONP;
 }));
